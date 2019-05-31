48f8421577e697a8d4c82dc39975ccabe3535425
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2019-05-31 21:10:39 +0530

    reordered according to website

--- C++/practice_problems/game_of_life/calls.h
+++
@@ -1,5 +0,0 @@
-void CopyMap(Grid_type, Grid_type);
-Boolean_type Enquire(void);
-void initialize(Grid_type);
-int NeighborCount(int,int,Grid_type);
-void WriteMap(Grid_type);
--- C++/practice_problems/game_of_life/lifedef.h
+++
@@ -1,6 +0,0 @@
-
-#define MAXROW 50
-#define MAXCOL 80
-
-typedef enum status_tag {DEAD, ALIVE} Status_type;
-typedef Status_type Grid_type[MAXROW][MAXCOL];
