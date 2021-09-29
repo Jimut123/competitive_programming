f79121c2f00ed3701a88101c490a7211de14b757
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2021-09-29 09:12:08 +0530

    Create 2004_gate_cs.c

---
+++ Code/MADE_EASY/2004_gate_cs.c
@@ -0,0 +1,15 @@
+/*This program prints nothing for some reason*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char p [20] ;
+    char *s="string";
+    int length = strlen(s);
+    for (int i = 0 ; i < length; i++)
+        p[i] = s[length  - i] ;
+    printf ("%s", p); 
+    return 0;
+}
