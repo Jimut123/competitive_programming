436be89a1c55c6feb59ba22bee71010170e76d27
Jimut123 <jimutbahanpal@yahoo.com>
2024-05-11 23:57:34 +0530

    2 probs solved

---
+++ LeetCode/2_add_two_num.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        # return as a Linked List
+        head = ListNode(0)
+        current = head
+        carry = 0
+        while l1 or l2:
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
+            sum = x + y + carry
+            carry = sum // 10
+            current.next = ListNode(sum % 10)
+            current = current.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+        if carry > 0:
+            current.next = ListNode(carry)
+        return head.next
---
+++ LeetCode/README.md
@@ -0,0 +1,5 @@
+ https://leetcode.com/discuss/general-discussion/460599/blind-75-leetcode-questions 
+
+ https://takeuforward.org/interviews/strivers-sde-sheet-top-coding-interview-problems/
+
+ 
